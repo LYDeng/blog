58d025bde583ad63bd51f549f9816dbf074ea411
LYDeng <youdeng123@qq.com>
2021-10-12 11:14:15 +0800

    提交

---
+++ config/database.js
@@ -0,0 +1,42 @@
+/*
+ * @Date: 2021-10-11 09:36:32
+ * @LastEditors: lyd
+ * @LastEditTime: 2021-10-12 10:59:15
+ * @Description: 无
+ */
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'blog'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'rootlyd'),
+        // ssl: {
+        //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        // },
+      },
+      options: {},
+    },
+  },
+});
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd5d56e34-df15-487f-a2a1-e348675cdd53'
+};
